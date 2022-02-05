ff45dceed489f3979789744475226fbee1c2f32e
rafael <rafaelsm003@gmail.com>
2022-02-05 13:25:55 -0300

    exercicios 4/3

--- fundamentos/bloco-4-introduçao-javascript-e-logica-de-programaçao/dia-3-logica-de-programaçao-e-algoritmos/script.js
+++ fundamentos/bloco-4-introduçao-javascript-e-logica-de-programaçao/dia-3-logica-de-programaçao-e-algoritmos/script.js
@@ -106,16 +106,19 @@ console.log("a menor palavra é: " + lowest);
 let primo = 0;
 let isPrime;
 
-for (let index = 0; index <= 50; index += 1) {
-    isPrime = true;
-  for (let index2 = 2; index2 < index; index2 += 1) {
-    if (index % index2 === 0) {
-      isPrime = false;
+for (let index = 0; index <= 50; index += 1){
+    if(index === 0 || index ===1){
+        isPrime = false;
+    } else{
+        isPrime = true;
     }
-  }
-  if (isPrime) {
-    primo = index;
-  }
+        for (let index2 = 2; index2 < index; index2 += 1){
+            if(index%index2==0){
+                isPrime = false;
+            }
+    }
+    if(isPrime){
+        primo += 1;
+    }
 }
-
 console.log(primo);

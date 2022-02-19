ab20e52cd1108cddcec36d38a0f27b9423e31ea5
rafael <rafaelsm003@gmail.com>
2022-02-19 00:13:19 -0300

    bloco6/dia1 exercicios

--- fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -2,26 +2,43 @@ const clearButton = document.querySelector("#clear-btn");
 const allowImage = document.querySelector('#allow-image');
 const submitBtn = document.querySelector('#submit-btn');
 
+function clearAll (){
+  const inputs = document.querySelectorAll("input");
+  const textArea = document.querySelector("textarea");
+  for (let index = 0; index < inputs.length; index +=1){
+    inputs[index].value = "";
+    inputs[index].checked = false;
+  }
+  textArea.value = "";
+}
 
-clearButton.addEventListener("click", function(){
-    const inputValues = document.querySelectorAll("input");
-    const textArea = document.querySelector("textarea");
-    for(let index = 0; index < inputValues.length; index += 1){
-        inputValues[index].value = "";
-        inputValues[index].checked = false;
-    }
-})
+function allowImageFunction (){
+  submitBtn.disabled = !allowImage.checked;
+}
 
-allowImage.addEventListener("change", function(){
-    submitBtn.disabled = !allowImage.checked;
-});
+function isValid (){
+  const name = document.querySelector("#full-name").value.length;
+  const email = document.querySelector("#email").value.length;
+  const textArea = document.querySelector("textarea");
+  if(email < 10 || email > 50 || name < 10 || name > 40 || textArea.value.length > 500){
+    return false;
+  } else {
+    return true;
+  }
+}
 
-submitBtn.addEventListener("click", function(){
-    const name = document.querySelector("#full-name");
-    const email = document.querySelector("#email");
-    if(name.value.length < 10 || name.value.length > 40 || email.value.length < 10 || email.value.length > 50){
-        alert("Dados invalidos!");
-    } else {
-        alert("Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip.");
-    }
-});
+function validationInput (event){
+  event.preventDefault();
+  const validation = isValid();
+  if(validation === false){
+    alert ("Dados inválidos");
+  } else {
+    alert("Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip.")
+  }
+}
+
+window.onload = function(){
+  clearButton.addEventListener("click", clearAll);
+  allowImage.addEventListener("change", allowImageFunction);
+  submitBtn.addEventListener("click", validationInput);
+}

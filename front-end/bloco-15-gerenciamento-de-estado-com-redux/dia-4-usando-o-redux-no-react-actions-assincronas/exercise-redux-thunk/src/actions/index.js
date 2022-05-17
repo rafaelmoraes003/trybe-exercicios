f3753fbae885345980ca2c13687bbc87608a5d34
rafael <rafaelsm003@gmail.com>
2022-05-17 11:41:19 -0300

    dia5/bloco15

---
+++ front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-4-usando-o-redux-no-react-actions-assincronas/exercise-redux-thunk/src/actions/index.js
@@ -0,0 +1,15 @@
+export const REQUEST_API = 'REQUEST_API';
+export const GET_PICTURE = 'GET_PICTURE';
+
+export const requestAPI = () => ({ type: REQUEST_API });
+
+export const getPicture = (data) => ({ type: GET_PICTURE, data });
+
+export const fetchAPI = () => {
+  return async (dispatch) => {
+    dispatch(requestAPI());
+    const response = await fetch('https://aws.random.cat/meow');
+    const data = await response.json();
+    dispatch(getPicture(data.file));
+  }
+}

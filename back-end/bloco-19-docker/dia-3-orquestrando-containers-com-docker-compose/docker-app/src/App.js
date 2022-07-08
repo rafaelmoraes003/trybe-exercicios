4ed9d29fdbd2f4a438af75b539a2f35a24e136d4
rafael <rafaelsm003@gmail.com>
2022-07-07 23:12:25 -0300

    exercicios docker compose

---
+++ back-end/bloco-19-docker/dia-3-orquestrando-containers-com-docker-compose/docker-app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 style={ { fontSize: "100px" } }>Docker</h1>
+      <p>Outro docker</p>
+      <p>mais um</p>
+    </div>
+  );
+}
+
+export default App;

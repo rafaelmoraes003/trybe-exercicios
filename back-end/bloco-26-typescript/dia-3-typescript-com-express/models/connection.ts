c32d67246a38208bb826506cab89967921ba8912
rafael moraes <rafaelsm003@gmail.com>
2022-09-12 21:00:04 -0300

    dia3/bloco26 - typescript com express

--- back-end/bloco-26-typescript/dia-3-typescript-com-express/models/connection.ts
+++ back-end/bloco-26-typescript/dia-3-typescript-com-express/models/connection.ts
@@ -3,6 +3,6 @@ import mysql from 'mysql2/promise';
 export const connection = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: '@Escop003',
+    password: 'xxxx',
     database: 'TypeScriptExpress',
 });

087ba0e7596f774f5ab5a106863c71bd3f1bd648
Rafael Moraes <rafaelsm003@gmail.com>
2022-11-25 18:45:28 -0300

    tipa parâmetros de funções

--- computer-science/dia-0-exercicios-extras-introduzindo-python/introduction-to-python/exercises/python_data_types.py
+++ computer-science/dia-0-exercicios-extras-introduzindo-python/introduction-to-python/exercises/python_data_types.py
@@ -30,35 +30,35 @@ def is_dict(value):
   return isinstance(value, dict)
 
 
-def return_lower_case_string(word = str):
+def return_lower_case_string(word: str):
   return word.lower()
 
 
-def append_element_in_list(element, input_list = list):
+def append_element_in_list(element, input_list: list):
   input_list.append(element)
   return input_list
 
 
-def remove_element_from_list(element, input_list = list):
+def remove_element_from_list(element, input_list: list):
   input_list.remove(element)
   return input_list
 
 
-def create_new_key_value_in_dict(key, value, input_dict = dict):
+def create_new_key_value_in_dict(key, value, input_dict: dict):
   input_dict[key] = value
   return input_dict
 
 
-def delete_key_from_dict(key, input_dict = dict):
+def delete_key_from_dict(key, input_dict: dict):
   input_dict.pop(key)
   return input_dict
 
 
-def add_element_to_set(element, input_set = set):
+def add_element_to_set(element, input_set: set):
   input_set.add(element)
   return input_set
 
 
-def remove_element_from_set(element, input_set = set):
+def remove_element_from_set(element, input_set: set):
   input_set.remove(element)
   return input_set

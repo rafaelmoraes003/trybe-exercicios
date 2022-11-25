3fe4eb60581351c193ad5cc31af41aeaa63d6b92
rafael moraes <rafaelsm003@gmail.com>
2022-11-25 17:24:46 -0300

    feat: dia0/bloco33 - introdução a python co exercícios

---
+++ computer_science/dia-0-exercicios-extras-introduzindo-python/introduction-to-python/exercises/basic_operations.py
@@ -0,0 +1,26 @@
+def basic_sum(first_number, second_number):
+  return first_number + second_number
+
+
+def basic_difference(first_number, second_number):
+  return first_number - second_number
+
+
+def basic_product(first_number, second_number):
+  return first_number * second_number
+
+
+def basic_division(first_number, second_number):
+  return first_number / second_number
+
+
+def basic_integer_division(first_number, second_number):
+  return first_number // second_number
+
+
+def basic_remainder(first_number, second_number):
+  return first_number % second_number
+
+
+def basic_potentiation(first_number, second_number):
+  return first_number ** second_number

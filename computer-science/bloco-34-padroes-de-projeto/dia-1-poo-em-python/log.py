d60561a04bae0210dd32c88b323083710d6b1cb6
Rafael Moraes <rafaelsm003@gmail.com>
2022-12-20 01:35:27 -0300

    Update log.py

--- computer-science/bloco-34-padroes-de-projeto/dia-1-poo-em-python/log.py
+++ computer-science/bloco-34-padroes-de-projeto/dia-1-poo-em-python/log.py
@@ -4,21 +4,19 @@ from datetime import datetime
 class ManipuladorDeLog(ABC):
     @classmethod
     @abstractmethod
-    def log(self, msg):
+    def log(cls, msg):
         raise NotImplementedError
-
 
 class LogEmTela(ManipuladorDeLog):
     @classmethod
-    def log(self, msg):
+    def log(cls, msg):
         print(msg)
-
 
 class LogEmArquivo(ManipuladorDeLog):
     @classmethod
-    def log(self, msg):
+    def log(cls, msg):
         with open("logs.txt", mode="a") as file:
-            print(f"{msg}\n", file=file)
+            print(msg, file=file)
 
 class Log:
     def __init__(self, manipuladores: list[ManipuladorDeLog]):
@@ -27,13 +25,13 @@ class Log:
     def adicionar_manipulador(self, manipulador):
         self.manipuladores.add(manipulador)
 
+    def __formatar(self, nivel, msg):
+        data = datetime.now().strftime('%d/%m/%Y %H:%M:%S')
+        return f"[{nivel} - {data}] - {msg}"
+
     def __log(self, nivel, msg):
         for manipulador in self.manipuladores:
             manipulador.log(self.__formatar(nivel, msg))
-
-    def __formatar(self, nivel, msg):
-        data = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-        return f"[{nivel} - {data}]: {msg}"
 
     def info(self, msg):
         self.__log("INFO", msg)
@@ -50,5 +48,4 @@ class Log:
 log_em_tela = LogEmTela()
 log_em_arquivo = LogEmArquivo()
 log = Log([log_em_tela, log_em_arquivo])
-log.erro("OSError - file not found.")
-
+log.erro("ZeroDivisionError - division by zero.")
